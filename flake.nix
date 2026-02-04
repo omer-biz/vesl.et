56f6e01f20a80546e63ecabd6ac50b5fea070c60
omer-biz <omerabdi@protonmail.com>
2026-02-04 22:11:51 +0300

    chore: set up a flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_24
+            typescript-language-server
+            typescript
+          ];
+        };
+      };
+    };
+}

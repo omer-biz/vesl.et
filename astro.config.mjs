4b3f1a3be4daf1a575b9e4e410920b53e6715666
omer-biz <omerabdi@protonmail.com>
2026-02-07 13:47:15 +0300

    feat: add reading time estimation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import { remarkReadingTime } from './remark-reading-time.ts';
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,6 +9,7 @@ export default defineConfig({
         plugins: [tailwindcss()],
     },
     markdown: {
-        shikiConfig: { theme: 'gruvbox-dark-hard' }
+        shikiConfig: { theme: 'gruvbox-dark-hard' },
+        remarkPlugins: [remarkReadingTime]
     },
 });

f6fd4021b0942eb2916e419dad5b523b5c0f4e16
omer-biz <omerabdi@protonmail.com>
2026-02-09 16:04:43 +0300

    feat: add an rss feed

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,18 @@
+import rss from '@astrojs/rss';
+import getLogsCollection from '../logsCollection';
+
+export async function GET(context) {
+    const logs = await getLogsCollection();
+
+    return rss({
+        title: 'Omer’s Blog',
+        description: 'just a curious mind venturing through the digital jungle.',
+        site: context.site,
+        items: logs.map((log) => ({
+            title: log.data.title,
+            pubDate: log.data.pubDate,
+            description: log.data.description,
+            link: `/logs/${log.id}`,
+        })),
+    });
+}
